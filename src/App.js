eaa6ef3e592f062c373c1d1f159e2829141d5422
杨晓伟 <yangxiaowei@yangxiaoweideMacBook-Pro.local>
2022-01-16 01:37:56 +0800

    feat:路由配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Button } from 'antd';
-import './App.less'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/login.jsx";
+import Admin from "./pages/admin/admin.jsx";
+
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
