ffcdb99fefe281fa21394029748bd41a302db576
杨晓伟 <yangxiaowei@yangxiaoweideMacBook-Pro.local>
2022-01-16 10:57:24 +0800

    头部样式+登录框

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./login.less";
+import logo from "./images/logo.png";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 export default class login extends Component {
-    render() {
-        return (
-            <div>
-                Login
-            </div>
-        )
-    }
+  render() {
+    var onFinish = (values) => {
+      console.log("Received values of form: ", values);
+    };
+    return (
+      <div className="login">
+        <header className="login-header">
+          <img src={logo} alt="logo"></img>
+          <h1>React项目：后台管理系统</h1>
+        </header>
+        <section className="login-content">
+          <h2>用户登录</h2>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Username!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    );
+  }
 }
